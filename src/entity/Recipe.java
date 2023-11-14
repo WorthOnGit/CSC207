e16ee6ff5c5f4719869c6e5f47a30f82f75898c8
worth <d.amir@mail.utoronto.ca>
2023-11-14 03:16:45 -0500

    fixing

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -1,45 +1,11 @@
 package entity;
 
-import java.util.HashMap;
-import java.util.List;
-
 public class Recipe {
 
-    private final HashMap<String, Object> recipe;
+    private final String Recipe;
 
-    public Recipe(HashMap<String, Object> recipe) {
-        this.recipe = recipe;
+    public Recipe(String recipe) {
+        Recipe = recipe;
     }
 
-    public String getRecipeName() {
-        return (String) recipe.get("recipeName");
-    }
-
-    public List<String> getIngredientLines() {
-        return (List<String>) recipe.get("ingredientLines");
-    }
-
-    public List<Object> getIngredients() {
-        return (List<Object>) recipe.get("ingredients");
-    }
-
-    public int getCalories() {
-        return (int) recipe.get("calories");
-    }
-
-    public CharSequence getHealthLabels() {
-        return (CharSequence) recipe.get("healthLabels");
-    }
-
-    public CharSequence getDietLabels() {
-        return (CharSequence) recipe.get("dietLabels");
-    }
-
-    public String getMealType() {
-        return (String) recipe.get("mealType");
-    }
-
-    public String getCuisineType() {
-        return (String) recipe.get("cuisineType");
-    }
 }
--- src/use_case/DataAccessInterface.java
+++ src/use_case/DataAccessInterface.java
@@ -1,9 +1,9 @@
 package use_case;
-import entity.Recipe;
 
+import entity.Recipe;
+import java.io.IOException;
 import java.util.List;
 
 public interface DataAccessInterface {
-
-    Recipe getRecipe(String recipeName, String countryoforigin, int Calories, List<String> dietLabels, List<String> healthLabels, String mealtype);
+    Recipe getRecipe(String recipename, String countryoforigin, int Calories, List<String> dietLabels, List<String> healthLabels, String mealtype) throws IOException;
 }
